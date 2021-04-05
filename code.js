8de41ad0b02b19a2cae65ffedc81aa5a5cc6f597
Stacy Read <stacy.read.007@gmail.com>
2021-04-05 16:57:24 -0500

    Changed from .keypress/.change to .on

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
 $(document).ready(
     function () {
-        $("#firstTextBox").change(textBoxWasChanged);
-        $("#secondTextBox").keypress(textBoxHadKeypress);
-
-        function textBoxWasChanged() {
-            // Generate a random number so we can see the change
-            var randomProof = Math.random();
-            $("#output").text(`First text box was changed: ${randomProof}`);
-        }
-
-        // Include event parameter to get information about the event
-        // that triggered this function to run
-        function textBoxHadKeypress(event) {
-            // Get the key that pressed from the event
-            var character = event.key;
-            $("#output").text(`Second text box heard a keypress: ${character}`);
-        }
+        $("#firstTextBox").on("change", textBoxWasChanged);
+        $("#secondTextBox").on("keypress", textBoxHadKeypress);
     }
 );
+
+function textBoxWasChanged() {
+    // Generate a random number so we can see the change
+    var randomProof = Math.random();
+    $("#output").text(`First text box was changed. Here's a random number to prove it: ${randomProof}`);
+}
+
+// Include event parameter to get information about the event
+// that triggered this function to run
+function textBoxHadKeypress(event) {
+    // Get the key that pressed from the event
+    var character = event.key;
+    $("#output").text(`Second text box heard a keypress. Here's the key that was typed: ${character}`);
+}
